6eb23bdb10648312c5cdcef25bda44baef6685bf
kawaiTakeru <t-kawai@bfts.co.jp>
2025-07-11 14:45:01 +0900

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+
 terraform {
   required_providers {
     azapi = {
@@ -52,4 +53,26 @@ resource "azapi_resource" "subscription" {
   }
 
   depends_on = [null_resource.check_billing_permission]
+}
+
+# ✅ リソースグループ作成
+resource "azurerm_resource_group" "vnet_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# ✅ 仮想ネットワーク作成
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.vnet_rg.location
+  resource_group_name = azurerm_resource_group.vnet_rg.name
+}
+
+# ✅ サブネット作成
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.vnet_rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
